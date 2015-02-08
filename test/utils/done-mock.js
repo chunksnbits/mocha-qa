e9dbdc522af9d7ac470328be736c800832eed3e6
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2015-02-08 05:19:13 +0100

    Updates error reporting for mock.

--- test/utils/done-mock.js
+++ test/utils/done-mock.js
@@ -7,13 +7,13 @@ module.exports = {
       if (arguments.length === 0) {
         return done();
       }
-      done(new Error('Failed the test on a negative result. Expected the promise to be resolved'));
+      done(arguments[0]);
     };
   },
   resolveOnError: function resolveOnError (done) {
     return function () {
       if (arguments.length === 0) {
-        return done(new Error('Failed the test on a positive result. Expected the promise to be rejected'));
+        return done(arguments[0]);
       }
       done();
     };
